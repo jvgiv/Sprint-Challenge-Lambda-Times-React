d09633139c5b67d1cf8eebdc7c37bd9185da1e6d
jvgiv <jvgiv12@gmail.com>
2019-03-29 14:23:04 -0400

    nearly done

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import PropTypes, { string } from 'prop-types';
+
 
 const Tab = props => {
+
+  console.log(props.selectTabHandler)
+  
+  
+    var doggy = ''
+    if (props.tab === props.selectedTab) {
+      doggy = 'tab active-tab'
+    } else {
+      doggy = 'tab'
+    }
+  
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
+      // console.log(props.tab)
   return (
+    
     <div
-      className={''}
+      className={doggy}
       onClick={() => {
+        props.selectTabHandler(props.tab)
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
     >
       {props.tab.toUpperCase()}
+
+      
     </div>
   );
 };
@@ -20,3 +39,7 @@ const Tab = props => {
 // Make sure you include PropTypes on your props.
 
 export default Tab;
+
+Tab.propTypes = {
+
+}
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes, { string } from 'prop-types';
+
+
+
+
+
 const Tabs = props => {
+
+  // console.log(props.tabs)
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
+        {props.tabs.map(eachTab => (
+          <Tab 
+            key={eachTab} 
+            tab={eachTab}
+            selectTabHandler={props.selectTabHandler} 
+            selectedTab={props.selectedTab}>
+              {eachTab.tabs}
+          </Tab>
+          
+        ))
+        
+          /* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
     </div>
